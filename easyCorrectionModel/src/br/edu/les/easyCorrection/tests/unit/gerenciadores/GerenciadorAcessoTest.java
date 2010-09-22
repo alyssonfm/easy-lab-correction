3120fa7fa1898cc4fcf006d8b578576cdf22f267
alyssonfilgueira <alyssonfilgueira@1fc89f1b-ef0b-fd1e-f970-6078fdf8a856>
2010-09-22 14:54:51 +0000

    Testes de Unidade para os Cadastros.

--- easyCorrectionModel/src/br/edu/les/easyCorrection/tests/unit/gerenciadores/GerenciadorAcessoTest.java
+++ easyCorrectionModel/src/br/edu/les/easyCorrection/tests/unit/gerenciadores/GerenciadorAcessoTest.java
@@ -1,24 +1,38 @@
 package br.edu.les.easyCorrection.tests.unit.gerenciadores;
 
-import junit.framework.TestCase;
+import br.edu.les.easyCorrection.exceptions.EasyCorrectionException;
 import br.edu.les.easyCorrection.gerenciadores.GerenciadorAcesso;
 import br.edu.les.easyCorrection.pojo.acesso.Usuario;
-
+import br.edu.les.easyCorrection.util.GeraMd5;
+import junit.framework.TestCase;
 
 public class GerenciadorAcessoTest extends TestCase{
 	
-	private Usuario u1;
 	private GerenciadorAcesso ga;
+	private Usuario u;
 	
 	public GerenciadorAcessoTest() {
-		u1 = new Usuario();
 		ga = new GerenciadorAcesso();
+		u = ga.getUsuario(3);
 	}
 	
-	public void testCriacaoUsuario(){
-		u1.setLogin("test1");
-		u1.setEmail("test@test.com");
-		u1.setIdUsuario(3);
-		ga.alterarSenhaAutomatico(u1.getIdUsuario());
+	public void test(){		
+		ga.alterarSenha(u, u.getLogin());
+		assertEquals(GeraMd5.md5(u.getLogin()), u.getSenha());
+		assertEquals("Alysson Filgueira", u.getNome());
+		assertEquals("alysson", u.getLogin());	
+		assertEquals("", u.getEmail());
+	}
+	
+	public void testAtualizacao(){
+		String email = "alyssonfilgueira@gmail.com";
+		u.setEmail(email);
+		assertEquals(email, u.getEmail());
+		try {
+			ga.atualizarUsuario(u);
+		} catch (EasyCorrectionException e) {
+			e.printStackTrace();
+		}
+		assertEquals(u, ga.consultarUsuarioPorLogin("alysson"));
 	}
 }
