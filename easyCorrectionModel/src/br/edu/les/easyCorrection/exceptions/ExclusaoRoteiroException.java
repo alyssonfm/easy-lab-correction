1c5476cdbb2f4757e7aa3a0489a775148fa73180
augustoqmacedo <augustoqmacedo@1fc89f1b-ef0b-fd1e-f970-6078fdf8a856>
2010-10-19 19:26:06 +0000

    Teste de unidades terminado =)

---
+++ easyCorrectionModel/src/br/edu/les/easyCorrection/exceptions/ExclusaoRoteiroException.java
@@ -0,0 +1,12 @@
+package br.edu.les.easyCorrection.exceptions;
+
+public class ExclusaoRoteiroException extends EasyCorrectionException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ExclusaoRoteiroException(String msg) {
+		super(msg);
+	}
+}
