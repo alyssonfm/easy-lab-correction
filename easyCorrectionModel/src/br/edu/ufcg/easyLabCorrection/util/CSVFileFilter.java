6d094f06fc180cfbe188e8e5cecd44a8a611c555
alyssonfilgueira <alyssonfilgueira@1fc89f1b-ef0b-fd1e-f970-6078fdf8a856>
2011-07-26 03:57:38 +0000

    Funcao que realiza a checagem do arquivo CSV e retorna uma lista de lista de strings (na realidade um arraylist de um arraylist de strings), está checando apenas a existencia dos 4 campos no arquivo: matricula, nome, email, turma. Demais checagens em breve ...

--- easyCorrectionModel/src/br/edu/ufcg/easyLabCorrection/util/CSVFileFilter.java
+++ easyCorrectionModel/src/br/edu/ufcg/easyLabCorrection/util/CSVFileFilter.java
@@ -1,13 +1,68 @@
 package br.edu.ufcg.easyLabCorrection.util;
 
+import java.io.*;
+import java.util.ArrayList;
+import br.edu.ufcg.easyLabCorrection.exceptions.EmptyFieldException;
+
 /**
- * The class responsible for checking of csv files.<br>
+ * Class responsible for checking the consistency of CSV files.<br>
  * @author Alysson Filgueira, Augusto Queiroz e Demetrio Gomes.<br>
  *
  */
 
 public class CSVFileFilter {
 	
+	private FileReader reader;
+	private BufferedReader br;
+	private String lineSeparator = ";";
+	private int lineNumber;
+	
+	/**
+	 * Constructor default of the class, creates a new object CSVFileFilter.<br>
+	 */
+	public CSVFileFilter() {
+		this.lineNumber = 0;
+	}
+	
+	/**
+	 * The function responsible for checking of CSV file.<br>
+	 * @param fileName The CSV file.<br>
+	 * @return The List of List of the Strings corresponding at the content of 
+	 * CSV file.<br>
+	 * @throws IOException The exception that can be launched in an attempt 
+	 * to open the file passed as parameter.<br>
+	 * @throws EmptyFieldException The exception that can be launched if the 
+	 * CSV file is not in accordance with the standard adopted.<br>
+	 */
+	public ArrayList<ArrayList<String>> checkCsvFile(String fileName) throws IOException, EmptyFieldException{
+		ArrayList<String> listLines = new ArrayList<String>();
+		ArrayList<ArrayList<String>> listAll = new ArrayList<ArrayList<String>>();
+		String line;
+		String[] linhas;
+		try {
+			this.reader = new FileReader(fileName);
+			br = new BufferedReader(reader);
+			while(br.ready()){
+				line = br.readLine();
+				linhas = line.split(lineSeparator);
+				lineNumber++;
+				if(linhas.length != 4){
+					throw new EmptyFieldException("Na linha " + lineNumber
+							+ " existe um numero invalido de campos.");
+				} else{
+					for(int i = 0; i < linhas.length; i++){
+						listLines.add(linhas[i]);
+					}
+					listAll.add(new ArrayList<String>(listLines));
+					listLines.clear();
+				}				
+			}
+			br.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return listAll;
+	}	
 	// pre condicoes - recebe arquivo file
 	// pos retornar lista de listas de strings.
 
